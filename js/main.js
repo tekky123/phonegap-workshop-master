5b0a3cd5f732b7dbb5c0023de8f3d48eeb87308d
Wojtek Chrobak <chrobakw@gmail.com>
2014-12-21 12:45:54 +0000

    added handlebars i views

--- js/main.js
+++ js/main.js
@@ -9,29 +9,24 @@ var app = {
     },
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
+    },
+    
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-    
-    initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 
 };
