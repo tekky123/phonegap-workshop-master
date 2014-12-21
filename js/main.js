5e7c73cec8cd63511928d52009f62801234ac630
Wojtek Chrobak <chrobakw@gmail.com>
2014-12-21 12:08:43 +0000

    added alert function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 var app = {
+    
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     findByName: function() {
         console.log('findByName');
@@ -15,6 +23,14 @@ var app = {
 
     initialize: function() {
         this.store = new MemoryStore();
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
